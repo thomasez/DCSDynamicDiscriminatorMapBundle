9061e8b5a857f9dbc01e6619d3fc347e80d5e6c8
Thomas Lundquist <thomasez@bisonlab.no>
2024-12-26 13:07:05 +0100

    Deprec

--- DependencyInjection/DCSDynamicDiscriminatorMapExtension.php
+++ DependencyInjection/DCSDynamicDiscriminatorMapExtension.php
@@ -4,8 +4,8 @@ namespace DCS\DynamicDiscriminatorMapBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration
